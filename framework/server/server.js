e305188d504072a09f405626fc26190384ef117b
ksah3756 <129350511+ksah3756@users.noreply.github.com>
2023-11-07 15:54:11 +0900

    test : Mern stack
    mern stack example

---
+++ framework/server/server.js
@@ -0,0 +1,25 @@
+// 설치한 express module을 불러와서 변수(express)에 저장
+const express = require("express");
+
+// mongodb 연결
+const connect = require('./schemas');
+connect();
+//express를 실행하여 app object를 초기화 
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+
+// 사용할 포트 번호를 port 변수에
+const = process.env.PORT || 5000;
+
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
+
+app.use(cors());
+app.use(express.json());
+
+// port변수를 이용하여 포트에 node.js 서버를 연결
+app.listen(port, () => {
+  console.log(`Server is now running on port : ${port}`);
+});
