8e55bde0f34e77c4a7c4807e2588613472247459
Hoona-Kim <101860831+Hoona-Kim@users.noreply.github.com>
2023-11-09 23:17:36 +0900

    webcam과 posenet을 이용하여 자세 인식. keypoint, skeleton 표시

---
+++ WebProject/posenet/src/App.js
@@ -0,0 +1,49 @@
+import React, { useRef } from "react";
+import "./App.css";
+import Webcam from "react-webcam";
+import {poseNet} from "./pose"
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  poseNet(webcamRef, canvasRef);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
