e5cf0d9b9b3fb5e0d7c4f9af6186968f70369687
Hoona-Kim <101860831+Hoona-Kim@users.noreply.github.com>
2023-11-17 18:55:14 +0900

    신체 각도 계산 구현 완료

---
+++ WebProject/posenet/src/math.js
@@ -0,0 +1,3 @@
+export function calculateAngle(A, B, C){
+    
+}
--- WebProject/posenet/src/pose.js
+++ WebProject/posenet/src/pose.js
@@ -15,7 +15,7 @@ export function poseNet(webcamRef, canvasRef) {
       //
       setInterval(() => {
         detect(net);
-      }, 100);
+      }, 1000); // 1000ms마다 자세 추정
     };
   
     const detect = async (net) => {
@@ -38,6 +38,7 @@ export function poseNet(webcamRef, canvasRef) {
         console.log(pose);
   
         drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+        calculateKeypoints(pose["keypoints"]); 
       }
     };
   
@@ -46,8 +47,72 @@ export function poseNet(webcamRef, canvasRef) {
       canvas.current.width = videoWidth;
       canvas.current.height = videoHeight;
   
-      drawKeypoints(pose["keypoints"], 0.6, ctx);
-      drawSkeleton(pose["keypoints"], 0.7, ctx);
+      drawKeypoints(pose["keypoints"], 0.5, ctx);
+      drawSkeleton(pose["keypoints"], 0.5, ctx);
+    };
+
+    const calculateKeypoints = (keypoints) => {
+      var rightSideIndex = 0;
+      if (keypoints[3].score < keypoints[4].score){
+        rightSideIndex = 1;
+      }
+
+      const keypoint_ear = keypoints[3 + rightSideIndex];
+      const keypoint_shoulder = keypoints[5 + rightSideIndex];
+      const keypoint_elbow = keypoints[7 + rightSideIndex];
+      const keypoint_wrist = keypoints[9 + rightSideIndex];
+      const keypoint_hip = keypoints[11 + rightSideIndex];
+      const keypoint_knee = keypoints[13 + rightSideIndex];
+      const keypoint_ankle = keypoints[15 + rightSideIndex];
+
+      var wrong_part = [];
+      var poseScore = 1;
+
+      // knee angle calculation
+      var angle_knee = calculate_angle(keypoint_hip.position, keypoint_knee.position, keypoint_ankle.position);
+      angle_knee = 180 - angle_knee;
+      //console.log("angle_knee: " + angle_knee);
+      if (angle_knee >= 90 && angle_knee <= 130){
+        wrong_part.push("knee");
+        poseScore += 1;
+      }
+
+      // Hip angle calculation
+      var angle_hip = calculate_angle(keypoint_shoulder.position, keypoint_hip.position, keypoint_knee.position);
+      //console.log("angle_hip: " + angle_hip);
+      if (angle_hip >= 90 && angle_hip <= 120){
+        wrong_part.push("hip");
+        poseScore += 1;
+      }
+
+      // neck angle calculation
+      var angle_neck = calculate_angle(keypoint_ear.position, keypoint_shoulder.position, keypoint_hip.position);
+      angle_neck = 180 - angle_neck;
+      //console.log("angle_neck " + angle_neck);
+      if (angle_neck >= 0 && angle_neck <= 15){
+        wrong_part.push("neck");
+        poseScore += 1;
+      }
+
+      // Elbow angle
+      var angle_elbow = calculate_angle(keypoint_shoulder.position, keypoint_elbow.position, keypoint_wrist.position);
+      //console.log("angle_elbow: " + angle_elbow);
+      if (angle_elbow >= 90 && angle_elbow <= 120){
+        wrong_part.push("elbow");
+        poseScore += 1;
+      }
+      //console.log("Pose Score: " + poseScore);
+
+    };
+
+    const calculate_angle =  (A, B, C) => {
+      var rad = Math.atan2(C["y"] - B["y"], C["x"] - B["x"]) - Math.atan2(B["y"] - A["y"], B["x"] - A["x"]);
+      if (rad < 0){
+        rad += Math.PI;
+      }
+      var deg = rad * (180 / Math.PI);
+
+      return deg;
     };
 
     runPosenet();
