4b9f2e2fc6132c76e7f82b167f940cebda7349c8
unknown <ojs9551@naver.com>
2023-12-06 22:02:15 +0900

    Fix api issue

--- framework/client/src/pages/LoginForm.js
+++ framework/client/src/pages/LoginForm.js
@@ -19,9 +19,13 @@ const LoginForm = () => {
     setPasswordError('');
 
     try {
-      const res = await axios.post('http://localhost:5000/login', { user_id: userId, password: password });
-      const data = res.data;
-      // console.log(data);
+      const res = await fetch('http://localhost:5000/login', {
+        method: 'POST',
+        body: JSON.stringify({ user_id: userId , password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const data = await res.json();
+      console.log(data);
       if (data.errors) {
         setUserIdError(data.errors.user_id);
         setPasswordError(data.errors.password);
@@ -30,7 +34,7 @@ const LoginForm = () => {
       if (data.user) { // 로그인 성공 시
         
         // 로그인 context 설정
-        setLoggedInUser(data.user);
+        setLoggedInUser(data.user_id);
 
         // 일단은 로그인 성공 시 원래 기본 페이지로 돌아가는데
         // 페이지를 더 추가해서 로그인 성공하면 http://localhost:3000/user_id 이런 페이지로 이동할 수 있도록?
